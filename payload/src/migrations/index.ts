281e5c1713759a67d893e47a9cf6657366044bed
T31K <t31kmunwong@gmail.com>
2026-08-10 18:29:19 +0800

    Revert "payload: create schema via committed migrations at startup"
    This reverts commit efc8ba6f791b8b79e6c48473edad942bd1460319.

--- payload/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260810_102431_initial from './20260810_102431_initial';
-
-export const migrations = [
-  {
-    up: migration_20260810_102431_initial.up,
-    down: migration_20260810_102431_initial.down,
-    name: '20260810_102431_initial'
-  },
-];
